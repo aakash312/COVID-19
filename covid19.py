8a9ca3bd3dc78224b389734cd540fbc9316fe84c
aakash312 <31676528+aakash312@users.noreply.github.com>
2020-06-19 00:25:44 -0400

    Final Changes

--- covid19.py
+++ covid19.py
@@ -30,20 +30,20 @@ class covid19:
                     json_obj1 = urlopen(val2)
                     data1 = json.load(json_obj1)
                     json_object1 = json.dumps(data1, indent=4)
-                    with open("D:\Python Outputs\API_US_Data.json",'w') as outfile1:
+                    with open('/Users/aakash/PycharmProjects/Output/API_US_Data.json','w') as outfile1:
                         outfile1.write(json_object1)
 
-                    self.df1.to_csv("D:\Python Outputs\ {}_API_US_Daily.csv".format(covid19.date_current))
+                    self.df1.to_csv("/Users/aakash/PycharmProjects/Output/{}_API_US_Daily.csv".format(covid19.date_current))
                 else:
                     # print('The json file generated will have the data from this URL: {}\n'.format(val2))
                     json_obj2 = urlopen(val2)
                     data2 = json.load(json_obj2)
                     json_object2 = json.dumps(data2, indent=4)
-                    with open("D:\Python Outputs\API_State_Data.json", "w") as outfile2:
+                    with open('/Users/aakash/Pycharmprojects/Output/API_State_Data.json', "w") as outfile2:
                         outfile2.write(json_object2)
 
-                    self.df2.to_csv("D:\Python Outputs\ {}_API_State_Current.csv".format(covid19.date_current))
-            print("apiData Function Executed Successfully\n")
+                    self.df2.to_csv("/Users/aakash/PycharmProjects/Output/ {}_API_State_Current.csv".format(covid19.date_current))
+                    print("apiData Function Executed Successfully\n")
         except:
             print("Error in apiData Function")
 
@@ -121,7 +121,7 @@ class covid19:
             plt.pie(x, labels=y, autopct='%1.1f%%')
             # plt.legend(y, loc="right")
             plt.title("Top 10 states that are most affected by COVID-19 are ")
-            plt.savefig('D:\Python Outputs\{}_Top_Most_Affected_State.pdf'.format(covid19.date_current))
+            plt.savefig('/Users/aakash/PycharmProjects/Output/{}_Top_Most_Affected_State.pdf'.format(covid19.date_current))
             # plt.show()
 
             x3 = self.df2.nsmallest(5, ['positive'])
@@ -130,7 +130,7 @@ class covid19:
             plt.pie(a, labels=b, autopct='%1.1f%%')
             # plt.legend(b, loc="right")
             plt.title("Top 10 states that are least affected by COVID-19 are ")
-            plt.savefig('D:\Python Outputs\{}_Top_Least_Affected_State.pdf'.format(covid19.date_current))
+            plt.savefig('/Users/aakash/PycharmProjects/Output/{}_Top_Least_Affected_State.pdf'.format(covid19.date_current))
             # plt.show()
 
             font = {'size': 65}
@@ -138,21 +138,22 @@ class covid19:
             plt.rcParams["figure.figsize"] = 60, 40
             self.df2.plot(kind='bar', x='state', y='positive', color='blue')
             plt.title("State wise positive cases on {}".format(covid19.date_current))
-            plt.savefig('D:\Python Outputs\{}_Positive_State.pdf'.format(covid19.date_current))
+            plt.savefig('/Users/aakash/PycharmProjects/Output/{}_Positive_State.pdf'.format(covid19.date_current))
             # plt.show()
 
             self.df2.plot(kind='bar', x='state', y='negative', color='red')
             plt.title("State wise negative cases on {}".format(covid19.date_current))
-            plt.savefig('D:\Python Outputs\{}_Negative_State.pdf'.format(covid19.date_current))
+            plt.savefig('/Users/aakash/PycharmProjects/Output/{}_Negative_State.pdf'.format(covid19.date_current))
             # plt.show()
 
             self.df2.plot(kind='bar', x='state', y='death', color='black')
             plt.title("State wise deaths cases on {}".format(covid19.date_current))
-            plt.savefig('D:\Python Outputs\{}_Deaths_State.pdf'.format(covid19.date_current))
+            plt.savefig('/Users/aakash/PycharmProjects/Output/{}_Deaths_State.pdf'.format(covid19.date_current))
             # plt.show()
             print('stateViz Function Executed Successfully\n')
 
         except:
+
             print('Error in stateViz Function')
 
     def usViz(self):
@@ -160,20 +161,55 @@ class covid19:
         This function takes df1 i.e. the currrent US data as input along with current date and generates PDF files with related visualizations
         '''
         try:
+            font = {'size': 35}
+            plt.rc('font', **font)
+            plt.rcParams["figure.figsize"] = 60, 60
+            self.df1.plot(kind='line', x='lastModified', y='hospitalizedCurrently', color='orange', linewidth=10)
+            plt.xticks(rotation=90)
+            plt.title("Patients in Hospital Currently in US on  {}".format(covid19.date_current))
+            plt.savefig('/Users/aakash/PycharmProjects/Output/{}_Hospitalized_US.pdf'.format(covid19.date_current))
+            # plt.show()
+
+            self.df1.plot(kind='line', x='lastModified', y='inIcuCurrently', color='red', linewidth=10)
+            plt.xticks(rotation=90)
+            plt.title("Patients in ICU Currently in US on  {}".format(covid19.date_current))
+            plt.savefig('/Users/aakash/PycharmProjects/Output/{}_ICU_US.pdf'.format(covid19.date_current))
+            # plt.show()
+
+
+            plt.rcParams["figure.figsize"] = 60, 60
+            self.df1.plot(kind='line', x='lastModified', y='onVentilatorCurrently', color='green', linewidth=10)
+            plt.xticks(rotation=90)
+            plt.title("Patients on Ventilator Currently in US on  {}".format(covid19.date_current))
+            plt.savefig('/Users/aakash/PycharmProjects/Output/{}_Ventilator_US.pdf'.format(covid19.date_current))
+            # plt.show()
+
+
+            font1 = {'size': 3.5}
+            plt.rc('font', **font1)
+            df_sorted = self.df1.sort_values('lastModified')
+            plt.plot('lastModified', 'hospitalizedCurrently', data=df_sorted, marker='o', markerfacecolor='black', markersize=2, color='skyblue', linewidth=2)
+            plt.plot('lastModified', 'inIcuCurrently', data=df_sorted, marker='', color='orange', linewidth=2)
+            plt.plot('lastModified', 'onVentilatorCurrently', data=df_sorted, marker='', color='red', linewidth=2, linestyle='dashed')
+            plt.xticks(rotation=90)
+            plt.legend()
+            plt.title("Hospital Related Stats for {}".format(covid19.date_current))
+            plt.savefig('/Users/aakash/PycharmProjects/Output/{}_All_Hospital_Related_Combined_US.pdf'.format(covid19.date_current))
+
             print('usViz Function Executed Successfully\n')
         except:
             print('Error in usViz Function')
 
 
 if __name__ == "__main__":
-    __covidViz__= covid19( df1 = pd.read_json("D:\Python Outputs\API_US_Data.json"),
-                  df2 = pd.read_json("D:\Python Outputs\API_State_Data.json"),
+    __covid__= covid19( df1 = pd.read_json("/Users/aakash/PycharmProjects/Output/API_US_Data.json"),
+                  df2 = pd.read_json("/Users/aakash/PycharmProjects/Output/API_State_Data.json"),
                   api_urls = {'key1': 'https://covidtracking.com/api/v1/us/daily.json',
                               'key2': 'https://covidtracking.com/api/v1/states/current.json'})
 
-    __covidViz__.apiData()
-    __covidViz__.dataExploration()
-    __covidViz__.stateData()
-    __covidViz__.usData()
-    __covidViz__.stateViz()
-    __covidViz__.usViz()
+    __covid__.apiData()
+    __covid__.dataExploration()
+    __covid__.stateData()
+    __covid__.usData()
+    __covid__.stateViz()
+    __covid__.usViz()
